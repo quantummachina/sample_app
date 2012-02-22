1cc76293543ba6969703ab38d279531a2f34dda6
Jorge F. Contreras <windblows3@yahoo.com>
2012-02-21 21:02:51 -0600

    Remove default Rails page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,36 +3,43 @@ require 'spec_helper'
 describe "Static pages" do
 
   describe "Home page" do
-  	it "Should have the h1 'Quantum Machina'" do
-  		visit '/static_pages/home'
-  		page.should have_selector('h1', :text => 'Quantum Machina')
+  	it "Should have the h1 'FunkApp'" do
+  		visit home_path
+  		page.should have_selector('h1', :text => 'FunkApp')
   	end
     it "should have the title 'Home'" do
-      visit '/static_pages/home'
+      visit home_path
       page.should have_selector('title', :text => "| Home")
     end
   end
 
   describe "Help page" do
   	it "should have the h1 'Help'" do
-  		visit '/static_pages/help'
+  		visit help_path
   		page.should have_selector('h1', :text => 'Help')
   	end
   	it "should have the title '| Help'" do
-  		visit '/static_pages/help'
+  		visit help_path
   		page.should have_selector('title', :text => '| Help')
   	end
   end
 
   describe "About page" do
   	it "should have the h1 'About'" do
-  		visit '/static_pages/about'
+  		visit about_path
   		page.should have_selector('h1', :text => 'About Us')
   	end
   	it "Should hace the title 'About Us'" do
-  		visit '/static_pages/about'
+  		visit about_path
   		page.should have_selector('title', :text => '| About Us')
   	end
   end
 
+  describe "Contact" do
+    it "should have the h1 and title 'Contact'" do
+      visit contact_path
+      page.should have_selector('h1', :text => 'Contact')
+      page.should have_selector('title', :text => '| Contact')
+    end
+  end
 end
