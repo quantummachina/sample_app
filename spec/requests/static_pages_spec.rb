889d98e03264542ca7398087e6ae052c62cc2a3e
Jorge F. Contreras <windblows3@yahoo.com>
2012-02-23 01:59:33 -0600

    Finish LLayout and routes 2

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,44 +2,30 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject { page }
+
   describe "Home page" do
-  	it "Should have the h1 'FunkApp'" do
-  		visit home_path
-  		page.should have_selector('h1', :text => 'FunkApp')
-  	end
-    it "should have the title 'Home'" do
-      visit home_path
-      page.should have_selector('title', :text => "| Home")
-    end
+    before { visit root_path }
+    it { should have_selector('h1', text: 'FunkApp')}
+    it { should have_selector('title', text: full_title('Home'))}
   end
 
   describe "Help page" do
-  	it "should have the h1 'Help'" do
-  		visit help_path
-  		page.should have_selector('h1', :text => 'Help')
-  	end
-  	it "should have the title '| Help'" do
-  		visit help_path
-  		page.should have_selector('title', :text => '| Help')
-  	end
+    before { visit help_path }
+  	it { should have_selector('h1', text: 'Help')}
+  	it { should have_selector('title', text: full_title('Help'))}
   end
 
   describe "About page" do
-  	it "should have the h1 'About'" do
-  		visit about_path
-  		page.should have_selector('h1', :text => 'About Us')
-  	end
-  	it "Should hace the title 'About Us'" do
-  		visit about_path
-  		page.should have_selector('title', :text => '| About Us')
-  	end
+    before { visit about_path}
+  	it { should have_selector('h1', text: 'About Us')}
+    it { should have_selector('title', text: full_title('About Us'))}
   end
 
   describe "Contact" do
-    it "should have the h1 and title 'Contact'" do
-      visit contact_path
-      page.should have_selector('h1', :text => 'Contact')
-      page.should have_selector('title', :text => '| Contact')
-    end
+    before { visit contact_path }
+    it { should have_selector('h1', text: 'Contact')}
+    it { should have_selector('title', text: full_title('Contact'))}
   end
+
 end
