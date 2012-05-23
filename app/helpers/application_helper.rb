911033f87a78908acf9a3450b46ab305ffdb2f37
Jorge F. Contreras <windblows3@yahoo.com>
2012-05-22 21:59:05 -0500

    managed auto_html for sizes on display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+	include AutoHtml
 
 	#Returns the full title on a per-page basis.
 	def full_title(page_title)
@@ -32,6 +33,14 @@ module ApplicationHelper
 		else
 			0
 		end
+	end
+
+
+
+	def embedded(content,size)
+		w=85*size
+		h=w*0.71
+		auto_html(content){ html_escape; image; vimeo(width: w, height: h); youtube(width: w, height: h); google_map(width: w, height: h); soundcloud(width: w, height: h); link target: "_blank", rel: "nofollow"; simple_format}
 	end
 
 end
